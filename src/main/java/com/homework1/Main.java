a084b5cca9467f2e382844000ba76b12899a87a8
Dimaoo7 <dimakrjukov@gmail.com>
2023-03-01 00:26:08 +0100

    Hw ended

--- src/main/java/com/homework1/Main.java
+++ src/main/java/com/homework1/Main.java
@@ -1,7 +1,23 @@
 package com.homework1;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Author Rifudzin = new Author("Рифудзин на Магонотэ");
+        Author Robert = new Author("Роберт Грин");
+
+       Book Robe = new Book(Robert, "48 законов власти ", 1998);
+
+        Book Rifud = new Book(Rifudzin, "Moshoku Tensei", 2012);
+
+        System.out.println("Книга " + Robe.getNameOfBook());
+
+        System.out.println("Автор " + Robe.getAuthor().getAuthor());
+
+        System.out.println("Год " + Robe.getPublishYear());
+
+        Robe.setPublishYear(2024);
+        System.out.println("Год " + Robe.getPublishYear());
+
     }
 }
